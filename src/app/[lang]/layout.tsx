8b8b554717107d5b5d4b70869200d2fc8d5a6bd4
Diogo <diogojorgepinto86@gmail.com>
2026-05-25 20:12:04 +0200

    Integra la tipografia Montserrat en todo el portal web

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,17 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Montserrat } from "next/font/google";
 import "../globals.css";
 import { LanguageProvider } from "@/context/LanguageContext";
 import { Language, translations } from "@/translations";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const montserrat = Montserrat({
+  variable: "--font-montserrat",
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["300", "400", "500", "700", "900"],
 });
 
 type Props = {
@@ -114,7 +110,7 @@ export default async function LocalizedLayout({ children, params }: Props) {
         />
       </head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-brand-dark`}
+        className={`${montserrat.variable} antialiased bg-brand-dark`}
       >
         <LanguageProvider defaultLanguage={lang}>
           {children}
